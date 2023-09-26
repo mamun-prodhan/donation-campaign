aeb979003c14e550a5f54fb8adaadc20daa8b635
mamun-prodhan <mamunpr9@gmail.com>
2023-09-26 09:22:30 +0600

    title and search bar added

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -11,10 +11,26 @@ const Home = () => {
   }, []);
   return (
     <div>
-      <h2 className="text-5xl font-bold">This is HOme Component</h2>
-      {donationData.map((donation, idx) => (
+      <div className="pt-36 pb-52">
+        <h1 className="text-5xl font-bold text-center">
+          I Grow By Helping People In Need
+        </h1>
+        <div className="text-center mt-10">
+          <input
+            className="w-96 p-4 border border-[#DEDEDE] focus:outline-none  rounded-l-lg "
+            type="text"
+            name="search"
+            id=""
+            placeholder="Search here...."
+          />
+          <button className="px-7 py-4 hover:bg-[#ff2b32] bg-[#FF444A] duration-300 rounded-r-lg border border-[#FF444A] font-semibold text-white">
+            Search
+          </button>
+        </div>
+      </div>
+      {/* {donationData.map((donation, idx) => (
         <li key={idx}>{donation.title}</li>
-      ))}
+      ))} */}
     </div>
   );
 };
