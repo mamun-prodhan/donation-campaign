e2f5f9b647ed2907ae1e06e6e25232e165d86fe9
mamun-prodhan <mamunpr9@gmail.com>
2023-09-27 14:45:13 +0600

    search functionality added

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,52 +1,24 @@
 import logo from "../../assets/Logo.png";
+import "./Header.css";
 import { NavLink, Link } from "react-router-dom";
 const Header = () => {
-  const links = (
-    <>
-      <li>
-        <NavLink to="/">Home</NavLink>
-      </li>
-      <li>
-        <NavLink to="/donation">Donation</NavLink>
-      </li>
-      <li>
-        <NavLink to="/statistics">Statistics</NavLink>
-      </li>
-    </>
-  );
   return (
-    <div className="navbar bg-base-100 mb-10 bg-[#0052FF26]">
-      <div className="navbar-start">
-        <div className="dropdown">
-          <label tabIndex={0} className="btn btn-ghost lg:hidden">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-5 w-5"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M4 6h16M4 12h8m-8 6h16"
-              />
-            </svg>
-          </label>
-          <ul
-            tabIndex={0}
-            className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
-          >
-            {links}
-          </ul>
-        </div>
-        <Link to="/">
-          <img src={logo} alt="logo" />
-        </Link>
+    <div className="flex items-center justify-between mb-36">
+      <div>
+        <img src={logo} alt="logo" />
       </div>
-      <div className="navbar-end hidden lg:flex">
-        <ul className="menu menu-horizontal px-1">{links}</ul>
+      <div>
+        <ul className="flex gap-12">
+          <li>
+            <NavLink to="/">Home</NavLink>
+          </li>
+          <li>
+            <NavLink to="/donation">Donation</NavLink>
+          </li>
+          <li>
+            <NavLink to="/statistics">Statistics</NavLink>
+          </li>
+        </ul>
       </div>
     </div>
   );
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,16 +1,35 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import DonationDetailsCard from "../DonationCard/DonationDetailsCard";
 
 const Home = () => {
   const [donationData, setDonationData] = useState([]);
+  const [displayData, setDisplayData] = useState([]);
+  const [searchData, setSearchData] = useState(""); // State for search query
+
+  const handleSearch = () => {
+    const searchedValue = searchData.toLowerCase();
+    console.log(searchedValue);
+    if (donationData) {
+      const findData = donationData.filter(
+        (data) => data.category.toLowerCase() === searchedValue
+      );
+      setDisplayData(findData);
+      console.log(displayData);
+    }
+    setSearchData("");
+  };
 
   useEffect(() => {
     fetch("data.json")
       .then((data) => data.json())
-      .then((data) => setDonationData(data));
+      .then((data) => {
+        setDonationData(data);
+        setDisplayData(data);
+      });
   }, []);
+
   return (
-    <div className="pt-36 ">
+    <div className="">
       <div className="pb-52">
         <h1 className="text-5xl font-bold text-center">
           I Grow By Helping People In Need
@@ -22,14 +41,22 @@ const Home = () => {
             name="search"
             id=""
             placeholder="Search here...."
+            value={searchData}
+            onChange={(e) => setSearchData(e.target.value)}
           />
-          <button className="px-7 py-4 hover:bg-[#ff2b32] bg-[#FF444A] duration-300 rounded-r-lg border border-[#FF444A] font-semibold text-white">
+          <button
+            onClick={handleSearch}
+            className="px-7 py-4 hover:bg-[#ff2b32] bg-[#FF444A] duration-300 rounded-r-lg border border-[#FF444A] font-semibold text-white"
+          >
             Search
           </button>
         </div>
       </div>
-      <div className="grid grid-cols-4 gap-6">
-        {donationData.map((donation, idx) => (
+      {displayData.length === 0 && (
+        <h2 className="text-center font-bold text-4xl mb-44">No Data Found</h2>
+      )}
+      <div className="grid grid-cols-4 gap-6 mb-44">
+        {displayData.map((donation, idx) => (
           <DonationDetailsCard
             key={idx}
             donation={donation}
